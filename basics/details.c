5d3911f1799997edc4966fed2951e5f70b03ec94
srikanthpragada <srikanthpragada@gmail.com>
2023-05-19 17:41:58 +0530

    19-MAY

---
+++ basics/details.c
@@ -0,0 +1,9 @@
+// Print details
+// Date : 19-MAY-2023
+
+main()
+{
+    printf("Srikanth Technologies\n");
+    printf("9059057000\n");
+    printf("contact@srikanthtechnologies.com");
+}
---
+++ basics/hello.c
@@ -0,0 +1,8 @@
+// My first C program
+// Date : 19-MAY-2023
+
+
+main()
+{
+  printf("Hello World!");
+}
